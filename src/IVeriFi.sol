d8d2b55d98b0427832f9632f0ea86d09e0be3676
ge-59 <gepiscopou@gmail.com>
2024-10-06 17:33:46 +0300

    Feat: Add error declarations to IVeriFi interface

--- src/IVeriFi.sol
+++ src/IVeriFi.sol
@@ -5,4 +5,11 @@ interface IVeriFi {
     event MerkleRootUpdated(bytes32 newRoot);
     event ManagerAdded(address newManager);
     event ManagerRemoved(address manager);
+
+    error Initialization_InvalidMerkleRoot();
+    error UpdateMerkleRoot_InvalidMerkleRoot();
+    error Initialization_InvalidAdminAddress();
+    error AddManager_InvalidManagerAddress();
+    error RemoveManager_InvalidManagerAddress();
+    error AuthorizeUpgrade_InvalidImplementationAddress();
 }
