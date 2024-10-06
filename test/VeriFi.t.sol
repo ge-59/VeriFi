887c965021aa45a9136cb052ec7eb1812c314f37
ge-59 <gepiscopou@gmail.com>
2024-10-06 18:25:56 +0300

    test: Add basic structure for VeriFi test file

---
+++ test/VeriFi.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {VeriFi} from "../src/VeriFi.sol";
+
+contract VeriFiTest is Test {
+    VeriFi public veriFi;
+
+    function setUp() public {
+    }
+
+}
