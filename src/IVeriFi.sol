b28bf69b3bb6d5971e991eadc89b55af389c8add
ge-59 <gepiscopou@gmail.com>
2024-10-06 17:45:00 +0300

    Docs: Add NatSpec comments for events in IVeriFi

--- src/IVeriFi.sol
+++ src/IVeriFi.sol
@@ -4,8 +4,20 @@ pragma solidity ^0.8.25;
 /// @title IVeriFi
 /// @dev Interface for VeriFi contract containing all events, errors and external functions
 interface IVeriFi {
+    /////////////////////////////////
+    ////////////  EVENTS  ///////////
+    /////////////////////////////////
+
+    /// @notice Emitted when the Merkle root is updated
+    /// @param newRoot The new Merkle root value
     event MerkleRootUpdated(bytes32 newRoot);
+
+    /// @notice Emitted when a new manager is added
+    /// @param newManager Address of the newly added manager
     event ManagerAdded(address newManager);
+
+    /// @notice Emitted when a manager is removed
+    /// @param manager Address of the removed manager
     event ManagerRemoved(address manager);
 
     error Initialization_InvalidMerkleRoot();
