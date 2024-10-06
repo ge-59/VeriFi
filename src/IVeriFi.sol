c265524614661e58f61f1425f5e6683d86b6df64
ge-59 <gepiscopou@gmail.com>
2024-10-06 17:28:45 +0300

    Feat: Add events to IVeriFi interface

--- src/IVeriFi.sol
+++ src/IVeriFi.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.25;
 
 interface IVeriFi {
-
-
-    
+    event MerkleRootUpdated(bytes32 newRoot);
+    event ManagerAdded(address newManager);
+    event ManagerRemoved(address manager);
 }
