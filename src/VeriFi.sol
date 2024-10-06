d13db01013ae858a5b47527e2f3aa87077d67994
ge-59 <gepiscopou@gmail.com>
2024-10-06 13:21:18 +0300

    Feat: Implement error check for invalid Merkle root in constructor

--- src/VeriFi.sol
+++ src/VeriFi.sol
@@ -6,7 +6,10 @@ import { MerkleProof } from "@oz/utils/cryptography/MerkleProof.sol";
 contract VeriFi {
     bytes32 public immutable MERKLE_ROOT;
 
+    error Constructor_InvalidMerkleRoot();
+
     constructor(bytes32 merkleRoot) {
+        if (merkleRoot == bytes32(0)) revert Constructor_InvalidMerkleRoot();
         MERKLE_ROOT = merkleRoot;
     }
 
